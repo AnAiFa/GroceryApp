e77112236bad3f1e45af6e3f9c94fb2185baa4f7
AnAiFa <vnacusa@gmail.com>
2020-02-25 20:29:33 +0800

    structure

---
+++ admin/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Clarisse Online Grocery</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <?php
+    require_once("login.php")
+  ?>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="../assets/js/jquery-3.4.1.min.js"></script>
+  <script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
