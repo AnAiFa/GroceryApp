60bb620b3c298c2e2269e6f5422b3f23e58f1c4f
AnAiFa <vnacusa@gmail.com>
2020-03-05 21:36:41 +0800

    remove source files

--- landing.php
+++ landing.php
@@ -1,21 +1,21 @@
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light justify-content-end">
+  <a class="navbar-brand mr-auto" href=""><img src="assets/img/grocery.jpg" alt="" height="47px" width="250px"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
 
-    <div class="collapse navbar-collapse text-center" id="navbarColor01">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="index"><img src="assets/img/grocery.jpg" alt="" height="47px" width="250px"></a>
-        </li>
-      </ul>
-      <form">
-        <button class="btn btn-outline-secondary" type="submit">Sign Up</button>
-        <button class="btn btn-outline-secondary" type="submit">Login</button>
-      </form>
+	<div class="collapse navbar-collapse" id="navbarColor01">
+		<div class="ml-auto my-3">
+			<button type="button" class="btn btn-outline-secondary" data-toggle="modal" data-target="#signUp">Sign Up</button>
+      <button type="button" class="btn btn-outline-secondary mr-2">Login</button>
     </div>
-  </nav>
+    <div class="form-inline form-label-group my-3">
+      <input type="text" id="inputSearch" class="form-control" placeholder="Search Here">
+      <label for="inputSearch">Search Here</label>
+    </div>
+	</div>
+</nav>
 
   <!-- Page Content -->
   <div class="container">
@@ -23,8 +23,7 @@
     <div class="row">
 
       <div class="col-lg-3">
-
-        <h1 class="my-4">Menu</h1>
+        <h2 class="my-3">All Category</h2>
         <div class="list-group">
           <a href="#" class="list-group-item">Category 1</a>
           <a href="#" class="list-group-item">Category 2</a>
@@ -44,13 +43,13 @@
           </ol>
           <div class="carousel-inner" role="listbox">
             <div class="carousel-item active">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+              <img class="d-block img-fluid" src="1.jpg" alt="First slide">
             </div>
             <div class="carousel-item">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+              <img class="d-block img-fluid" src="2.jpg" alt="Second slide">
             </div>
             <div class="carousel-item">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+              <img class="d-block img-fluid" src="3.jpg" alt="Third slide">
             </div>
           </div>
           <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -179,3 +178,31 @@
       <p class="m-0 text-center">Copyright &copy; Your Website 2020</p>
     </div>
   </footer>
+
+  <div class="modal fade" id="signUp" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Sign Up</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="form-label-group">
+            <input type="text" id="inputFullName" class="form-control" placeholder="Full Name" required="">
+            <label for="inputFullName">Full Name</label>
+          </div>
+          <div class="form-label-group">
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="">
+            <label for="inputEmail">Email address</label>
+          </div>
+          
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-outline-primary">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  </div>
